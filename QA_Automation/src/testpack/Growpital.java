3e18fdf3decb599d18b545cad5b75df91ef111c9
neilr <neilr@Neil>
2023-04-20 09:11:03 +0530

    running Instructions added

--- QA_Automation/src/testpack/Growpital.java
+++ QA_Automation/src/testpack/Growpital.java
@@ -28,31 +28,44 @@ public class Growpital {
 		
 		explicitWait(driver,5,"(//a[@href='https://app.growpital.com/login'])[1]");
 		
+		
+		//This will wait for 5 sec and click on email button
+		
 		explicitWait(driver,5,"//input[@type='email']");
+		
+		//This will wait for 5 sec and click on emailInput section
 		
 		WebElement emailInput = driver.findElement(By.xpath("//input[@type='email']"));
 		
+		//once we've are on email Input I've entered the values
+		
 		emailInput.sendKeys("neilrajurkar95@gmail.com");
+		
+		//after finding element below code will help to enter password 
 		
 		WebElement passInput = driver.findElement(By.className("password-input"));
 		
 		passInput.sendKeys("Liverpool@01");
 		
+		//Below code will help to click on Login
+		
 		WebElement login = driver.findElement(By.xpath("//div[text()='Login']"));
 		
 		login.click();
 		
+		//Below code is to show we can also use Delay (Thread.sleep) and Clicked on svg element to bring View profile 
 		Thread.sleep(10000);
 		
 		WebElement navDropDown = driver.findElement(By.xpath("(//*[local-name()='svg'])[3]"));
 		
 		navDropDown.click();
 		
+		//Below is the code to click on View Profile Option
 		WebElement viewProfile = driver.findElement(By.xpath("//a[@class='popup-content']"));
 		
 		viewProfile.click();
 		
-		//Below is the code to make sure I'm on a Personal Information Section
+		//Below is the code to make sure we are on a Personal Information Section
 		
 		WebElement personalInfo = driver.findElement(By.xpath("//div[@class='form-card-title']"));
 		
@@ -62,9 +75,11 @@ public class Growpital {
 		
 		System.out.println(text + " " + verifySectionofPerosanalInfo);
 		
+		//below is the code to wait for 5 sec and then click on Upload Documents tab
+		
 		explicitWait(driver,5,"(//button[@class='custom-button unVerified'])[3]");
 		
-		//Below is the code to make sure I'm on a Upload Your Document Section
+		//Below is the code to make sure we are on a Upload Your Document Section
 		WebElement uploadDocSection = driver.findElement(By.xpath("(//div[@class='form-card-title'])[1]"));
 		
 		String text2 = uploadDocSection.getText();
@@ -73,6 +88,7 @@ public class Growpital {
 		
 		System.out.println(text2 + " " +verifyUploadDocSection);
 		
+		//this code will help to close the browser
 		driver.quit();
 		
 	}
